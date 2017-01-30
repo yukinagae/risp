786ac4e9ad982e6ac701ba09310efbb77298efe7
Yuki <yuki.nagae1130@gmail.com>
2017-01-30 16:54:09 +1100

    second commit

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,102 @@
 extern crate regex;
 
 use regex::Regex;
+use std::fmt;
+use Sexp::Nil;
+use Sexp::Integer;
+use Sexp::Float;
+use Sexp::Symbol;
+use std::ops::Add;
+
+#[derive(Debug)]
+enum Sexp {
+    Nil,
+    Integer { value: i32 },
+    Float { value: f32 },
+    Symbol { name: String, value: String }
+}
+
+// enum Atom {
+//     Integer,
+//     Float,
+//     Symbol
+// }
+
+// trait Number {
+//     fn to_string(&self) -> String;
+// }
+
+// impl Add for Integer {
+//     type Output = Integer;
+//     fn add(self, other: Integer) -> Integer {
+//         Integer {
+//             value: self.value + other.value
+//         }
+//     }
+// }
+
+// impl Add for Float {
+//     type Output = Float;
+//     fn add(self, other: Float) -> Float {
+//         Float {
+//             value: self.value + other.value
+//         }
+//     }
+// }
+
+// impl PartialEq for Integer {
+//     fn eq(&self, other: &Integer) -> bool {
+//         self.value == other.value
+//     }
+// }
+
+// impl PartialEq for Float {
+//     fn eq(&self, other: &Float) -> bool {
+//         self.value == other.value
+//     }
+// }
+
+// impl fmt::Debug for Number {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         write!(f, "{}", self.to_string())
+//     }
+// }
+
+#[derive(Debug)]
+struct Cell { car: Sexp, cdr: Sexp }
+
+impl Cell {
+    fn new() -> Cell {
+        let cell = Cell { car: Nil, cdr: Integer { value: 1 } };
+        return cell;
+    }
+}
+
+// #[derive(Debug)]
+// struct Integer { value: i32 }
+
+// impl Number for Integer {
+//     fn to_string(&self) -> String { self.value.to_string() }
+// }
+
+// #[derive(Debug)]
+// struct Float { value: f32 }
+
+// impl Number for Float {
+//     fn to_string(&self) -> String {
+//         return self.value.to_string();
+//     }
+// }
+
+// struct Symbol { name: String, value: String }
 
 fn main() {
-    let stdin = "(begin (define r 10) (* pi (* r r)))";
-    let tokens = tokenize(&stdin);
+    let cell = Cell::new();
+    println!("{:?}", cell);
+    // let stdin = "(begin (define r 10) (* pi (* r r)))";
+    // let tokens = tokenize(&stdin);
 
-    println!("{:?}", tokens);
+    // println!("{:?}", tokens);
 }
 
 #[derive(Debug)]
